51aeca397a4bf69a49bbca0fcf06a235944bfd35
Keerthi <keerthana14298@gmail.com>
2023-09-12 11:10:15 +0530

    Common css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './css/common.css';
+import 'font-awesome/css/font-awesome.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Menu from './menu';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Menu/>
   </React.StrictMode>
 );
 
---
+++ src/menu.js
@@ -0,0 +1,25 @@
+function menu() {
+  return (
+    <>
+      <br></br>
+      <button className="btns">
+        All Projects &nbsp;<span className="fa fa-arrow-right"></span>
+      </button>
+      <h1 className="main-title">We Offer a Wide Variety of IT Services</h1>
+      <h1 className="sub-title">We Offer a Wide Variety of IT Services</h1>
+      <h1 className="mini-title main-color">Welcome Infygain</h1>
+      <h1 className="mini-title sec-color">Welcome Infygain</h1>
+      <div className="top-banner">
+        <div>
+          <p className="main-title">Welcome Infygain</p>
+          <p className="mini-title">About us</p>
+        </div>
+      </div>
+      <a href="#" className="links">About us</a>
+      <a href="#" className="icon-pri"><span className="fa fa-instagram"></span> </a>
+      <a href="#" className="icon-sec"><span className="fa fa-instagram"></span> </a>
+    </>
+  );
+}
+
+export default menu;

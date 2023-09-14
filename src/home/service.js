9f5587ab5e29b07464c531744009f476317c7a76
Ragandev <ranjith44.hhh@gmail.com>
2023-09-14 18:50:08 +0530

    Service, About and Sub Service Modules Done

--- src/home/service.js
+++ src/home/service.js
@@ -1,5 +1,50 @@
+import React from "react";
 import "../css/home.css";
+import "animate.css";
 
 const Service = () => {
-    re
-}
+  return (
+    <>
+      <div className="serviceContainer container-lg text-center">
+        <div className="serviceTopBox">
+            <p className="mini-title main-color">Our Service</p>
+            <p className="sub-title">We Offer a Wide Variety of IT Services</p>
+        </div>
+      <div className="row serviceBox">
+        <div className="serviceBoxInn col-lg-4">
+          <div className="serviceCont">
+            <img src="/images/icons/it.png" alt="service icons"></img>
+            <h2 className="mid-title">IOT Services</h2>
+            <p className="text-muted">
+              Expertise in a IT consultancy for the many companies, for their
+              different working areas.
+            </p>
+          </div>
+        </div>
+        <div className="serviceBoxInn col-lg-4">
+          <div className="serviceCont">
+            <img src="/images/icons/web.png" alt="service icons"></img>
+            <h2 className="mid-title">Web Development</h2>
+            <p className="text-muted">
+              Expertise in a IT consultancy for the many companies, for their
+              different working areas.
+            </p>
+          </div>
+        </div>
+        <div className="serviceBoxInn col-lg-4">
+          <div className="serviceCont">
+            <img src="/images/icons/isp.png" alt="service icons"></img>
+            <h2 className="mid-title">ISP Services</h2>
+            <p className="text-muted">
+              Expertise in a IT consultancy for the many companies, for their
+              different working areas.
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Service;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import 'font-awesome/css/font-awesome.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Sliders from './home/slider';
+import Service from './home/service';
+import About from './home/about';
+import SubService from './home/subService';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Sliders />
+    <Service />
+    <About />
+    <SubService />
   </React.StrictMode>
 );

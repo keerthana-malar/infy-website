0cde4fcdfa4d57952b74064cb466378342517818
Keerthi <keerthana14298@gmail.com>
2023-09-19 16:40:01 +0530

    about page
    work done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './css/common.css';
+import './css/header.css';
+import './css/about.css';
 import 'font-awesome/css/font-awesome.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Menu from './menu';
+import Header from './header';
+import About from './about';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Menu/>
+    <Header/>
+    <About/>
   </React.StrictMode>
 );
 

8dc6b1b157be57d3d59e9e6b66e357616e88c382
Keerthi <keerthana14298@gmail.com>
2023-09-30 17:53:44 +0530

    Webpage content update finished

--- src/services/spotlight.js
+++ src/services/spotlight.js
@@ -15,13 +15,12 @@ function webservice() {
         </div>
         <div className="sercon">
           <p className="su-title">
-            Major Services that will<br></br>
+            Major Services That Will<br></br>
           </p>
           <span className="stt">Grow Your Business</span>
           <p className="head-conn">
-            As a SEO Advisor or web crawler expert, I help organizations
-             adjust to the expanding significance of internet
-            promoting.
+            These services are essential for a robust online presence and
+            effective digital engagement.
           </p>
         </div>
         <div className="mserv">
@@ -31,50 +30,74 @@ function webservice() {
             </div>
             <div className="st">Websites</div>
             <div className="scon text-muted">
-                A website is a collection of web pages and related digital
-                content that is accessible via the internet.
+              We provide custom website design and development solutions that
+              are not only visually striking but also optimized for performance
+              and user experience and elevate your brand with us.
             </div>
             <div className="smore smb1">
-                <a href=""><span>Contact us &nbsp;<HiArrowLongRight/></span></a>
+              <a href="">
+                <span>
+                  Contact us &nbsp;
+                  <HiArrowLongRight />
+                </span>
+              </a>
             </div>
           </div>
           <div className="sb sbox2">
             <div className="simgbox">
-              <img className="fluid" src="./images/seo/app.png"></img>
+              <img className="fluid" src="./images/seo/seo.png"></img>
             </div>
             <div className="st">SEO</div>
             <div className="scon text-muted">
-                A website is a collection of web pages and related digital
-                content that is accessible via the internet.
+              Our goal is to drive organic traffic, boost your online authority,
+              and help you reach a wider audience, ultimately leading to
+              increased brand recognition and higher conversion rates.
             </div>
             <div className="smore smb2">
-                <a href=""><span>Contact us  &nbsp;<HiArrowLongRight/></span></a>
+              <a href="">
+                <span>
+                  Contact us &nbsp;
+                  <HiArrowLongRight />
+                </span>
+              </a>
             </div>
           </div>
           <div className="sb sbox3">
             <div className="simgbox">
-              <img className="fluid" src="./images/seo/seo.png"></img>
+              <img className="fluid" src="./images/seo/app.png"></img>
             </div>
             <div className="st">Applications</div>
             <div className="scon text-muted">
-                A website is a collection of web pages and related digital
-                content that is accessible via the internet.
+              Web application service is all about bringing your ideas to life
+              with cutting-edge technology. We develop custom web applications
+              that cater to your specific business needs and goals.
             </div>
             <div className="smore smb3">
-                <a href=""><span>Contact us  &nbsp;<HiArrowLongRight/></span></a>
+              <a href="">
+                <span>
+                  Contact us &nbsp;
+                  <HiArrowLongRight />
+                </span>
+              </a>
             </div>
           </div>
-          <div className="sb sbox4"> 
+          <div className="sb sbox4">
             <div className="simgbox">
               <img className="fluid" src="./images/seo/graphic.png"></img>
             </div>
             <div className="st">Designing</div>
             <div className="scon text-muted">
-                A website is a collection of web pages and related digital
-                content that is accessible via the internet.
+              We craft captivating visuals, from logos to marketing materials,
+              that align with your brand identity and engage your audience. We
+              offer a wide range of design solutions to elevate branding.
             </div>
             <div className="smore smb4">
-                <a href=""><span>Contact us  &nbsp;<HiArrowLongRight/></span></a>
+              <a href="">
+                <span>
+                  Contact us &nbsp;
+                  <HiArrowLongRight />
+                </span>
+              </a>
             </div>
           </div>
         </div>

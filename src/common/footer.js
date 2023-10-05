0bde6c7b7c36ce13b207e4ba52d33a4ad7a02674
sathish140602 <111893172+sathish140602@users.noreply.github.com>
2023-10-04 17:48:17 -0700

    footer section responsive updated

--- src/common/footer.js
+++ src/common/footer.js
@@ -13,28 +13,28 @@ function Footer() {
                 <div className='container'>
                 <div className='small-box '>
                     <div className='row small'>
-                        <div className='col-lg-3 mb-3 name'>
+                        <div className='col-lg-3  name'>
                             <p className='infy'>Infygain</p>
                         </div>
-                        <div className='col-lg-3 mb-3 nam'>
+                        <div className='col-lg-3 col-md-4 col-sm-12  nam nam1'>
                             <p className='call'><b>CALL FOR SUPPORT</b></p>
                             <p className='phone'>9952141444</p>
 
                         </div>
 
-                        <div className='col-lg-3 mb-3 nam'>
+                        <div className='col-lg-3 col-md-4 col-sm-12 nam'>
                             <h1 className='infyy'><b>Follow Us</b><br></br></h1>
                             <p className='hari text-muted'> Hari complex opp.prozone mall,
                                 Sathy Rd, Saravanampatti,
                                 Coimbatore, Tamil Nadu 641035</p>
                         </div>
-                        <div className='col-lg-3 mb-3 nam-icon'>
+                        <div className='col-lg-3 col-md-4 col-sm-12  nam-icon'>
                             <h1 className='infyy-icon'><b>Follow Us </b><br></br></h1>
                             <div className='infyicon'>
-                            <a href="#" className="infyicon"><i className="fa fab fa-facebook"></i></a>
-                            <a href="#" className="infyicon"><i className="fa fab fa-twitter"></i></a>
-                            <a href="#" className="infyicon"><i className="fa fab fa-instagram"></i></a>
-                            <a href="#" className="infyicon"><i className="fa fab fa-youtube"></i></a>
+                            <a href="#" className="infyicon infyicon1"><i className="fa fab fa-facebook"></i></a>
+                            <a href="#" className="infyicon infyicon1"><i className="fa fab fa-twitter"></i></a>
+                            <a href="#" className="infyicon infyicon1"><i className="fa fab fa-instagram"></i></a>
+                            <a href="#" className="infyicon infyicon1"><i className="fa fab fa-youtube"></i></a>
                             </div>
                         </div>
 
@@ -47,24 +47,27 @@ function Footer() {
                         <div className='col-xs-6 col-sm-3 mb-3 '>
                             <h1 className='mini-title one'>Our Services</h1>
                             <ul className='text-muted '>
-                                <li>Complete Network Solutions</li>
+                                {/* <li>Complete Network Solutions</li>
                                 <li> Laptop / Desktop Sales and Service</li>
                                 <li>CCTV / Biometrics Sales and Service</li>
                                 <li> Web Development / Graphic Designs</li>
                                 <li> Printers / Devices</li>
-                                <li>POS / Billing Software</li>
+                                <li>POS / Billing Software</li> */}
+                                <li>IT Supports</li>
+                                <li>ISP</li>
+                                <li>Web / App</li>
                             </ul>
                         </div>
                         <div className='col-xs-6 col-sm-3'>
                             <h1 className='mini-title two'>Quick Links</h1>
                             <ul className='text-muted quicklinks mb-5'>
-                                <li> <Link to="/">Home</Link></li>
+                                {/* <li> <Link to="/">Home</Link></li> */}
                                 <li> <Link to="/about">About Us</Link></li>
-                                <li> <Link to="/service">Services</Link></li>
+                                {/* <li> <Link to="/service">Services</Link></li> */}
                                 <li><Link to="/blog">Blog</Link></li>
                                 <li> <Link to="/contact">Contact Us</Link></li>
                                 <li> <Link to="/contact">Online Store</Link></li>
-                                <li> <Link to="/contact">Book Services</Link></li>
+                                {/* <li> <Link to="/contact">Book Services</Link></li> */}
                             </ul>
                         </div>
                         <div className='col-xs-6 col-sm-3'>

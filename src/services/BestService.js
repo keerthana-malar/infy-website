e969e54197908fd89f787d0f8d6aacb2ed56d40d
kiruthikaor <132900132+kiruthikaor@users.noreply.github.com>
2023-10-12 18:29:53 -1100

    finished work

---
+++ src/services/BestService.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../css/BestService.css';
+
+
+function BestService(){
+    return(
+        <div className='container'>
+            <div className='row'>
+            <div className='col-md-8'>
+                <div className='firstlineflex'>
+                <div className='yellowbreak'></div>
+                <div className='firstheading'>
+                   <h1>We give the best Services </h1>
+                   </div>
+            </div>
+            <div className='text-muted'>
+            <p>There are many variations of passages of 
+                Lorem Ipsum available, but the majority have suffered alteration
+                 in some form, by injected humour, or randomised words which don’t 
+                 look even slightly believable. If you are going to use a passage of
+                  Lorem Ipsum, you need to be sure there isn’t anything embarrassing hidden.
+                  </p>
+                  </div>
+            </div>
+            <div className='col-md-4'>
+                <div className='rightboxheadcol'>
+                <div className='rightbox'> Social Media Marketing</div>
+                <div className='rightbox'> Social Media Marketing</div>
+                <div className='rightbox'> Social Media Marketing</div>
+                <div className='rightbox'> Social Media Marketing</div>
+                <div className='rightbox'> Social Media Marketing</div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default BestService;

f89602d93d23edbd4e4e7eac6589a452a6807405
sathish140602 <111893172+sathish140602@users.noreply.github.com>
2023-11-02 12:14:06 -0700

    blogs

--- src/home/HomeBlog.js
+++ src/home/HomeBlog.js
@@ -2,9 +2,61 @@ import "../css/home.css";
 import { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
 import axios from "axios";
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';  
 
 const HomeBlog = () => {
     const [blogs, setBlogs] = useState([]);
+
+    var settings = {
+      infinite: true,
+      dots: false, 
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      arrows:false,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      adaptiveHeight: true,
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: false,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 1
+            }
+          },
+          {
+              breakpoint: 400,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                initialSlide: 1
+              }
+            }
+        ]
+  };
 
   useEffect(() => {
     axios.get("https://infygain.in/api/blogdata").then((res) => {
@@ -26,24 +78,32 @@ console.log(blogs[0])
                 <p className="mini-title main-color">From Our Blogs</p>
                 <p className="test-head">Here’s What’s Happening</p>
             </div>
-            <div className="homeBlogBox">
-                <div className="row p-1">
-            {blogs.map((value, index)=>(
-                    <div key={value.id} className="col-md-4">
-                        <div className="blogBoxInn">
-                            <div className="blogImgBox">
-                                <img className="img-fluid" src={"uploads/" + value.img}></img>
-                            </div>
-                            <div className="blogContBox">
-                                <p className="text-muted">{dateSlice(index)}</p>
-                                <p className="mid-title ">{value.title}</p>
-                                <a className="links"><Link to={"/blogs/"+value.id}>READ MORE</Link></a>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-                </div>
+            
+              <div className="homeBlogBox">
+              <Slider {...settings}>
+        {blogs.map((value, index) => (
+          <div key={value.id} className="col-md-4">
+            <div className="blogBoxInn">
+              <div className="blogImgBox">
+              <a className="links" href='/mainblogs'>
+                  <img className="img-fluid" src={`uploads/${value.img}`} alt={value.title} />  
+                </a>
+              </div>
+              <div className="blogContBox">
+                <p className="text-muted">{dateSlice(index)}</p>
+                <p className="mid-title">{value.title}</p>
+                <a className="links" href='/mainblogs'>
+                  READ MORE
+                </a>
+                {/* Alternatively, you can use React Router's Link component:
+                <Link to={`/blogs/${value.id}`}>READ MORE</Link> */}
+              </div>
             </div>
+          </div>
+        ))}
+      </Slider>
+              </div>
+           
         </div>
       </section>
     </>

a99859e7e31506d213d4353c8a6e93076c89d6ac
indhumathi740 <114278440+indhumathi740@users.noreply.github.com>
2023-12-12 11:52:09 +0530

    links error clearing

--- src/contact.js
+++ src/contact.js
@@ -63,39 +63,8 @@ function Contactus() {
           content="ocR-tZ4yt2LD1ePYD1bR4LK6GYUc0keDJcspEtblHoM"
         />
         {/* End Robot tag */}
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            className="tag-iframe"
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NRCN7RF"
-            height="0"
-            width="0"
-            title="Google Tag Manager"
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-        {/* Facebook Pixel Code */}
-        <script type="text/javascript">
-          {`
-               !function(f,b,e,v,n,t,s)
-               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-               n.queue=[];t=b.createElement(e);t.async=!0;
-               t.src=v;s=b.getElementsByTagName(e)[0];
-               s.parentNode.insertBefore(t,s)}(window, document,'script',
-               'https://connect.facebook.net/en_US/fbevents.js');
-               fbq('init', '1093378958018811');
-               fbq('track', 'PageView');
-               `}
-        </script>
-        <noscript>{`
-               <img height="1" width="1" style="display:none"
-               src="https://www.facebook.com/tr?id=1093378958018811&ev=PageView&noscript=1"
-               />
-               `}
-        </noscript>
-        {/* End Facebook Pixel Code */}
+       
+      
         {/* Organization schema*/}
 
         <script type="application/ld+json">
